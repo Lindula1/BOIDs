36c0601b91d2c8a62c56d47f9f4a6aff89ecb4eb
LindulaG <66986963+Lindula1@users.noreply.github.com>
2026-09-21 19:53:31 +1000

    fix

--- pygame_implementation.py
+++ pygame_implementation.py
@@ -1,6 +1,5 @@
 import pygame
 from boid_model import Population
-from gpu_optimised import GPUPopulation
 from numba import cuda
 
 print(cuda.is_available())
@@ -21,21 +20,14 @@ frame = 0
 
 boid_size = 2
 
-<<<<<<< Updated upstream
-test_pop = Population((0, HEIGHT, 0, WIDTH), 100, frame_rate=1 / 1, fixed_seed=True, safe_distance=2,
-                      turn_factor=0.2, separation=0.05, alignment=0.05, visible_distance=20, max_speed=3, min_speed=2,
-                      cohesion=0.005)
-test_pop.load_boids()
-=======
 border_adjust = 2
 border = (border_adjust, int(HEIGHT-border_adjust), border_adjust, int(WIDTH-border_adjust))
 margin = tuple(int(border[_] + 50 * (-1) ** _) for _ in range(len(border)))
 
-test_pop = GPUPopulation(border, 200, margin, frame_rate=1 / 1, safe_distance=4,
+test_pop = Population(border, 200, frame_rate=1 / 1, safe_distance=4,
                       turn_factor=0.4, separation=0.05, alignment=0.05, visible_distance=30, max_speed=6, min_speed=2,
                       cohesion=0.003, rebound_factor=0.9)
-# test_pop.load_boids()
->>>>>>> Stashed changes
+test_pop.load_boids()
 
 while running:
     for event in pygame.event.get():
